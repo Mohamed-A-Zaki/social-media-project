4d2fff3ec1f68d9c9667786d480676890c50685a
Mohamed Zaki <87452598+Mohamed-A-Zaki@users.noreply.github.com>
2023-08-13 02:18:32 +0300

    upload

--- src/components/PostDetails/Comment.tsx
+++ src/components/PostDetails/Comment.tsx
@@ -1,29 +1,34 @@
 import { CommentType } from "../../types/Post.type";
-import { Card, Avatar, Typography, Paper } from "@mui/material";
+import { Avatar, Typography, Stack } from "@mui/material";
 
 const Comment = ({ author, body }: CommentType) => {
-  const { profile_image, name, username } = author;
+  const { profile_image, name, username, email } = author;
 
   return (
-    <Card
-      elevation={2}
-      sx={{ p: 2, my: 2, ml: 12, display: "flex", gap: 2, bgcolor: "#f2f2f2" }}
+    <Stack
+      direction="row"
+      spacing={2}
+      sx={{ p: 2, my: 2, ml: 12, bgcolor: "#f2f2f2", borderRadius: 2 }}
     >
       <Avatar
         sx={{ width: 50, height: 50, border: 1, borderColor: "#ccc" }}
         src={typeof profile_image === "string" ? profile_image : undefined}
       />
 
-      <Paper sx={{ p: 2, width: 1 }}>
-        <Typography variant="subtitle2" fontSize={17} fontStyle={"italic"}>
+      <Stack direction="column" bgcolor="#fff" borderRadius={2} width={1} p={2}>
+        <Typography variant="subtitle2" fontSize={17}>
           {`${username} - ${name}`}
         </Typography>
 
-        <Paper sx={{ bgcolor: "#fafafa", mt: 2, p: 2 }} elevation={2}>
+        <Typography variant="body2" color={"text.secondary"}>
+          {email}
+        </Typography>
+
+        <Typography bgcolor="#fafafa" mt={2} p={2} borderRadius={2}>
           {body}
-        </Paper>
-      </Paper>
-    </Card>
+        </Typography>
+      </Stack>
+    </Stack>
   );
 };
 
